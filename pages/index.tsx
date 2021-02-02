772967f67ba79dc1341c92e91efee42c933d2bb3
nobu <pyomta13@gmail.com>
2021-02-03 04:42:37 +0900

    ts to js

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,10 +3,7 @@ import Head from "next/head";
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>テスト</title>
-      </Head>
-      <p>あいさつ</p>
+    test
     </>
   );
 }
